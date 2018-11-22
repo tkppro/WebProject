147ebf336d0c8fbaa0c8c594c80d6ef11bf43ffa
ThangDang <tkpproo@gmail.com>
2018-11-22 09:29:07 +0700

    fix connection

--- WebProject/connection.php
+++ WebProject/connection.php
@@ -1,16 +1,11 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "wolfclaw1";
+$password = "";
 $dbname = "vnuk";
-
-// Create connection
+ // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-// else
-//     echo 'Connected successfuly';
-//mysqli_close($conn);
-?>
